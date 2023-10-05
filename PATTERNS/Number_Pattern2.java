af522ae16c5183706b5a7f71c692d4ad94456dad
PATHIPATI CHIRANJEEVI <prudhvivenkatachiranjeevi@gmail.com>
2023-10-05 09:40:19 +0530

    "New"

---
+++ PATTERNS/Number_Pattern2.java
@@ -0,0 +1,29 @@
+package PATTERNS;
+
+import java.util.Scanner;
+
+public class Number_Pattern2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        for (int i = 0; i <2*n-1; i++) {
+            for (int j = 0; j <2*n-1 ; j++) {
+                int t=i;
+                int w=j;
+                if(i>=n) t=n-t;
+                else if(j>=n)  w=n-w;
+
+            }
+        }
+        for (int i = 0; i < 2*n-1; i++) {
+            for (int j = 0; j <2*n-1; j++) {
+                int top = i;
+                int bottom = j;
+                int right = (2*n - 2) - j;
+                int left = (2*n - 2) - i;
+                System.out.print(n- Math.min(Math.min(top,bottom), Math.min(left,right)) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
